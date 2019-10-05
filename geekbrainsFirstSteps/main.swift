de5a4d37d2edb2b90834d1938bd1941c664b8131
Nikita Boiko <genertyniki@gmail.com>
2019-10-05 09:16:11 +0300

    task 1 done

--- geekbrainsFirstSteps/main.swift
+++ geekbrainsFirstSteps/main.swift
@@ -8,5 +8,42 @@
 
 import Foundation
 
-print("Hello, World!")
+func getNumber() -> Int {
+    let maxBoard: UInt32 = 20
+    var currentValue = Int(arc4random_uniform(maxBoard) + 1)
+    currentValue = currentValue * (arc4random_uniform(4)>0 ? 1 : -1) //max 4, because need biggeer percent of positive numbers
+    return currentValue
+}
+
+func getRoot(a: Int, b: Int, d: Int, negative: Bool) -> String {
+    var currentRoot: Double = 0
+    if negative {
+        currentRoot = (Double((-1)*b) + sqrt(Double(d)))/Double(2*a)
+    } else {
+        currentRoot = (Double((-1)*b) - sqrt(Double(d)))/Double(2*a)
+    }
+    return String(format: "%.3f", currentRoot)
+}
+
+var a: Int = getNumber()
+var b: Int = getNumber()
+var c: Int  = getNumber()
+
+print("\(a)*xˆ2\(b<0 ? "" : "+")\(b)*x\(c<0 ? "" : "+")\(c) = 0")
+
+let d = b*b - 4*a*c
+print(d)
+if (d>0) {
+    print("Дискриминант больше нуля. Корня два")
+    let xOne = getRoot(a: a, b: b, d: d, negative: false)
+    let xTwo = getRoot(a: a, b: b, d: d, negative: true)
+    print("Ответ: Корень 1 = \(xOne), Корень 2 = \(xTwo)")
+} else if (d == 0) {
+    print("Дискриминант равен нулю. Корень один")
+    let xOne = getRoot(a: a, b: b, d: d, negative: false)
+    print("Ответ: Корень 1 = \(xOne)")
+} else {
+    print("Дискриминант меньше нуля. Корней нет")
+}
+
 
