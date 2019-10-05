ddb1c289b53b349438df80f04ed55e7c6cc91250
Nikita Boiko <genertyniki@gmail.com>
2019-10-05 07:37:51 +0300

    Initial Commit

---
+++ geekbrainsFirstSteps/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  geekbrainsFirstSteps
+//
+//  Created by Nikita Boiko on 05/10/2019.
+//  Copyright © 2019 Nikita Boiko. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
